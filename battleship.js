8a33e8631f05a847dd00cdf284eaa43b5f4760c3
rerdos <ricsi96@gmail.com>
2015-07-16 15:03:23 +0100

    User input validator

--- battleship.js
+++ battleship.js
@@ -2,24 +2,42 @@ var ships = [];
 
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
+var guessedCoords = [];
+
 placeShip(5);
 placeShip(4);
 placeShip(4);
 
-var prompt = require('prompt');
-
-var userInput;
-
-prompt.start();
-
-prompt.get(['coordinate'], function (err, result) {
-    console.log("jfoisdhfoisdiufhsdiofhisdhjf");
-    console.log('coordinate: ' + result.coordinate);
-    userInput = 4;
-
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
 });
+console.log(ships);
+console.log("To start the game, input a coordinate (for example 'F5'):");
 
-console.log(userInput);
+rl.on('line', function(line) {
+    if (isGameOver()){
+        console.log("You have finished the game!");
+        process.exit();
+    }
+    if (isValidInput(line)) {
+        if (isGuessed(line)) {
+            console.log("You have already guessed that coordinate!");
+        } else {
+            guessedCoords.push(line);
+            if (isExist(line)) {
+                console.log("You've just bombed that coordinate!");
+                setCoordinateToX(line);
+            } else {
+                console.log("There was nothing on that coordinate!");
+            }
+        }
+    } else {
+        console.log("You have entered an invalid coordinate!");
+    }
+});
 
 function placeShip(length) {
     var coordinates = [];
@@ -104,4 +122,43 @@ function isExist(coordinate) {
         }
     }
     return false;
+}
+
+function isGameOver() {
+    for (var i = 0; i < ships.length; i++) {
+        for (var j = 0; j < ships[i].length; j++) {
+            if (ships[i][j] !== 'x') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function isGuessed(coordinate) {
+    for (var i = 0; i < guessedCoords.length; i++) {
+        if (coordinate === guessedCoords[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function setCoordinateToX(coordinate) {
+    for (var i = 0; i < ships.length; i++) {
+        for (var j = 0; j < ships[i].length; j++) {
+
+            if (ships[i][j] === coordinate) {
+                ships[i][j] = 'x';
+                isShipSink(i);
+            }
+        }
+    }
+
+    function isShipSink(currentShip) {
+        for (var i = 0; i < ships[currentShip].length && ships[currentShip][i] === 'x'; i++) {}
+        if (i === ships[currentShip].length) {
+            console.log("You have sunk that ship!");
+        }
+    }
 }
