0eb572f7aada3b3b7c795bb7892e044aeede5e99
rerdos <ricsi96@gmail.com>
2015-07-16 11:34:55 +0100

    made placeShip() function work

--- battleship.js
+++ battleship.js
@@ -1,28 +1,63 @@
-var ships = [
-    ['A1', 'A2', 'A3', 'A4', 'A5']
-];
+var ships = [];
 
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
+placeShip(5);
+placeShip(4);
+placeShip(4);
+
+console.log(ships);
+
 function placeShip(length) {
+    var coordinates = [];
+    var coordinate;
+    var i;
     var isHorizontal = Math.random() < 0.5;
     var newShip;
+
     if (isHorizontal) { 
     //horizontal 
-        var coordinates = [];
-        var coordinate;
         do {
             newShip = [];
-            coordinates[0] = Math.floor(Math.random() * (10 - length + 1));
-            coordinates[1] = Math.floor(Math.random() * 10) + 1;
-            coordinate = letters[coordinates[0]] + coordinates[1];
-            newShip[newShip.length] = coordinate;
-        } while (isExist(coordinate));
+            for (i = 0; i < length; i++) {
+                if(i === 0) {
+                    coordinates[0] = Math.floor(Math.random() * (10 - length + 1));
+                    coordinates[1] = Math.floor(Math.random() * 10) + 1;
+                    coordinate = letters[coordinates[0]] + coordinates[1];
+                    newShip[newShip.length] = coordinate;
+                } else {
+                    coordinate = letters[coordinates[0]+i] + coordinates[1];
+                    newShip[newShip.length] = coordinate;
+                }
+            }
+        } while (!isValidPlace(newShip));
     } else { 
         //vertical 
-        console.log('vertical');
+        do {
+            newShip = [];
+            for (i = 0; i < length; i++) {
+                if(i === 0) {
+                    coordinates[0] = Math.floor(Math.random() * 10);
+                    coordinates[1] = Math.floor(Math.random() * (10 - length + 1)) + 1;
+                    coordinate = letters[coordinates[0]] + coordinates[1];
+                    newShip[newShip.length] = coordinate;
+                } else {
+                    coordinate = letters[coordinates[0]] + (coordinates[1]+i);
+                    newShip[newShip.length] = coordinate;
+                }
+            }
+        } while (!isValidPlace(newShip));
     }
-    console.log(newShip);
+    ships[ships.length] = newShip;
+}
+
+function isValidPlace(ship) {
+    for (var i = 0; i < ship.length; i++) {
+        if (isExist(ship[i])) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function isExist(coordinate) {
