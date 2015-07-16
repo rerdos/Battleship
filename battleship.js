8f878894125554c2b976fdc8463c7afa2e609eb3
rerdos <ricsi96@gmail.com>
2015-07-16 11:03:57 +0100

    placeShip(length) is added - create a new starting point

--- battleship.js
+++ battleship.js
@@ -4,6 +4,27 @@ var ships = [
 
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
+function placeShip(length) {
+    var isHorizontal = Math.random() < 0.5;
+    var newShip;
+    if (isHorizontal) { 
+    //horizontal 
+        var coordinates = [];
+        var coordinate;
+        do {
+            newShip = [];
+            coordinates[0] = Math.floor(Math.random() * (10 - length + 1));
+            coordinates[1] = Math.floor(Math.random() * 10) + 1;
+            coordinate = letters[coordinates[0]] + coordinates[1];
+            newShip[newShip.length] = coordinate;
+        } while (isExist(coordinate));
+    } else { 
+        //vertical 
+        console.log('vertical');
+    }
+    console.log(newShip);
+}
+
 function isExist(coordinate) {
     for (var i = 0; i < ships.length; i++) {
         for (var j = 0; j < ships[i].length; j++) {
